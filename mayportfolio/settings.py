87a1e1b01b9c58f1337a118889069e989bed3071
Stewart Milton <136900060+stewartmilton59@users.noreply.github.com>
2026-03-16 01:30:35 +0300

    Update settings.py

--- mayportfolio/settings.py
+++ mayportfolio/settings.py
@@ -15,9 +15,9 @@ SECRET_KEY = 'django-insecure-!@dg7473sj0wwmj0seoh!uqtt2(8c0u0_s5od+5z2x7(%qw6fv
 #    SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['mayportfolio.onrender.com', 'www.mayportfolio.onrender.com']
 #ALLOWED_HOSTS = ['ameno.pythonanywhere.com']
 #CSRF_TRUSTED_ORIGINS = ['https://ameno.pythonanywhere.com']
 
